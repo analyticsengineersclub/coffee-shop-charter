5a0d1e64b2fd63ccc486fc86d5cf401fbb4695c4
Ubuntu <ubuntu@ip-172-31-18-98.us-east-2.compute.internal>
2021-10-23 17:18:28 +0000

    first staging model added

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,21 @@
+with orders as (
+        select
+                customer_id,
+                min(created_at) as first_order,
+                count(*) as count
+        from `analytics-engineers-club.coffee_shop.orders
+       group by 1
+),
+customers as (
+        select *
+        from `analytics-engineers-club.coffee_shop.customers`
+)
+
+select 
+        customers.id,
+        customers.name,
+        customers.email,
+        orders.first_order,
+        orders.count
+from customers
+left join orders on customers.id = orders.customer_id
